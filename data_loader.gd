64a43afac3109bd4b9f6c9089dbe05e2ee6e0022
Alexandru Săndulescu <alexandru.prahova@gmail.com>
2025-10-17 14:58:23 -0400

    data_loader help

--- data_loader.gd
+++ data_loader.gd
@@ -1,1 +1,24 @@
 extends Node
+
+var data: Dictionary
+
+func load_from_csv():
+	var path = "res://points_csv.txt"
+	var contents = FileAccess.open(path, FileAccess.READ).get_as_text()
+	
+	var lines = contents.split("\n")
+	var columns = lines[0].split(",")
+	
+	for column in columns:
+		data[column] = Array()
+	
+	for lidx in range(1, len(lines)):
+		var vals = lines[lidx].split(",")
+		if len(vals) >= len(columns):
+			for cidx in range(len(columns)):
+				data[columns[cidx]].append(vals[cidx])
+
+
+func _ready() -> void:
+	# Call the load csv function in order to load the data
+	pass

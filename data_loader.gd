7046349924c175e6118dfea5b17fcb20020170d5
Alexandru Săndulescu <alexandru.prahova@gmail.com>
2025-10-17 15:27:22 -0400

    removed solution

--- data_loader.gd
+++ data_loader.gd
@@ -20,9 +20,8 @@ func load_from_csv():
 
 
 func _ready() -> void:
-	# Call the load csv function in order to load the data
-	
-	load_from_csv()
+	# Call the load csv function in order to load the data:
+	# load_from_csv()
 	
 	print(data)
 	
@@ -34,12 +33,4 @@ func spawn_points() -> void:
 	# Relevant docs:
 	# - https://docs.godotengine.org/en/stable/tutorials/scripting/nodes_and_scene_instances.html
 	
-	for i in range(500):
-		var new_point = preload("res://point_scene.tscn").instantiate()
-		add_child(new_point)
-		new_point.position = Vector3(float(data["x"][i]), float(data["y"][i]), float(data["z"][i]))
 	pass
-
-
-func _on_h_slider_value_changed(value: float) -> void:
-	self.transform = Basis().scaled(Vector3(value * 0.1, 1, 1))
